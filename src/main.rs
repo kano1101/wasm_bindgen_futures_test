0828e2637c5cf6134fe43a7a2d0f9ea1c47eeb4a
Akira Kano <a.kano1101@gmail.com>
2022-12-04 10:38:31 +0900

    リファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use log::{debug, error, info, trace, warn};
-
 use serde::Deserialize;
 #[derive(Deserialize)]
 struct Ip {
@@ -7,18 +5,12 @@ struct Ip {
 }
 
 mod space {
-    use anyhow::Result;
     use once_cell::sync::Lazy;
     use tokio::sync::Mutex;
 
     static INSTANCE: Lazy<Mutex<Option<String>>> = Lazy::new(|| Mutex::new(None));
 
-    pub async fn init() -> Result<()> {
-        let mut lock = INSTANCE.lock().await;
-        *lock = None;
-        Ok(())
-    }
-    pub async fn pull() -> Result<()> {
+    pub async fn pull() -> anyhow::Result<()> {
         use super::Ip;
         let response = reqwest::get("http://httpbin.org/ip")
             .await?
@@ -36,26 +28,27 @@ mod space {
 
 fn run() {
     console_error_panic_hook::set_once();
-
     wasm_logger::init(wasm_logger::Config::default());
 
-    trace!("some trace log");
-    debug!("some debug log");
-    info!("some info log");
-    warn!("some warn log");
-    error!("some error log");
+    log::trace!("some trace log");
+    log::debug!("some debug log");
+    log::info!("some info log");
+    log::warn!("some warn log");
+    log::error!("some error log");
 
     let fut = || async move {
-        assert!(space::get().await.is_none());
-        assert!(space::init().await.is_ok());
         assert!(space::get().await.is_none());
         assert!(space::pull().await.is_ok());
         let origin = space::get().await;
         assert!(origin.is_some());
-        debug!("ip.origin: {:?}", origin);
+        log::debug!("ip.origin: {:?}", origin);
     };
 
     wasm_bindgen_futures::spawn_local(fut());
+}
+
+fn main() {
+    run();
 }
 
 use wasm_bindgen_test::wasm_bindgen_test;
@@ -64,9 +57,5 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 async fn first_test() {
-    run();
-}
-
-fn main() {
     run();
 }
