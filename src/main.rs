c6303764d0e2502c60b7dba45773380dc719c550
Akira Kano <a.kano1101@gmail.com>
2022-12-04 20:51:53 +0900

    ひとまず動作させてみた

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,64 @@ mod space {
     use once_cell::sync::Lazy;
     use tokio::sync::Mutex;
 
-    static INSTANCE: Lazy<Mutex<Option<String>>> = Lazy::new(|| Mutex::new(None));
+    #[derive(Debug, Clone)]
+    pub enum Perform<T> {
+        None,
+        Yet(T),
+        Already(T),
+    }
+    impl<T> Default for Perform<T> {
+        fn default() -> Perform<T> {
+            Perform::None
+        }
+    }
+    impl<T> From<Perform<T>> for Option<T> {
+        fn from(from: Perform<T>) -> Option<T> {
+            let perform = from;
+            match perform {
+                Perform::None => Option::None,
+                Perform::Yet(_) => Option::None,
+                Perform::Already(t) => Some(t),
+            }
+        }
+    }
+    impl<T> Perform<T> {
+        pub fn is_none(&self) -> bool {
+            if let Perform::None = self {
+                return true;
+            }
+            return false;
+        }
+        pub fn is_yet(&self) -> bool {
+            if let Perform::Yet(_) = self {
+                return true;
+            }
+            return false;
+        }
+        pub fn is_already(&self) -> bool {
+            if let Perform::Already(_) = self {
+                return true;
+            }
+            return false;
+        }
+    }
 
+    static INSTANCE: Lazy<Mutex<Vec<Perform<String>>>> = Lazy::new(|| Mutex::new(Vec::new()));
+
+    pub async fn get() -> Perform<String> {
+        let lock = INSTANCE.lock().await;
+        if let Some(first) = lock.first() {
+            return first.clone();
+        }
+        return Perform::None;
+    }
+    pub async fn add() -> anyhow::Result<()> {
+        let mut lock = INSTANCE.lock().await;
+        if lock.len() == 0 {
+            lock.push(Perform::Yet("テスト中".to_string()));
+        }
+        Ok(())
+    }
     pub async fn pull() -> anyhow::Result<()> {
         use super::Ip;
         let response = reqwest::get("http://httpbin.org/ip")
@@ -17,12 +73,10 @@ mod space {
             .json::<Ip>()
             .await?;
         let mut lock = INSTANCE.lock().await;
-        *lock = Some(response.origin);
+        if let Some(first) = lock.first_mut() {
+            *first = Perform::Already(response.origin);
+        }
         Ok(())
-    }
-    pub async fn get() -> Option<String> {
-        let lock = INSTANCE.lock().await;
-        lock.clone()
     }
 }
 
@@ -38,9 +92,11 @@ fn run() {
 
     let fut = || async move {
         assert!(space::get().await.is_none());
+        assert!(space::add().await.is_ok());
+        assert!(space::get().await.is_yet());
         assert!(space::pull().await.is_ok());
         let origin = space::get().await;
-        assert!(origin.is_some());
+        assert!(origin.is_already());
         log::debug!("ip.origin: {:?}", origin);
 
         // 当然ですがコメントアウトを外すと、localhost:8080からの実行ではエラーになる
