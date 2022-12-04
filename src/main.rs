14445cee14a482c397bbb0fbfc582d31983191c1
Akira Kano <a.kano1101@gmail.com>
2022-12-04 10:31:35 +0900

    リファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,40 @@
 use log::{debug, error, info, trace, warn};
 
 use serde::Deserialize;
-#[derive(Clone, Deserialize)]
+#[derive(Deserialize)]
 struct Ip {
     origin: String,
 }
 
-#[derive(Debug, Clone)]
-enum Perform<T> {
-    None,
-    Yet(T),
-    Already(T),
-}
-impl<T> Default for Perform<T> {
-    fn default() -> Perform<T> {
-        Perform::None
-    }
-}
-impl<T> From<Perform<T>> for Option<T> {
-    fn from(from: Perform<T>) -> Option<T> {
-        let perform = from;
-        match perform {
-            Perform::None => Option::None,
-            Perform::Yet(_) => Option::None,
-            Perform::Already(t) => Some(t),
-        }
-    }
-}
-
 mod space {
-    use super::{Ip, Perform};
+    use anyhow::Result;
     use once_cell::sync::Lazy;
     use tokio::sync::Mutex;
-    static INSTANCE: Lazy<Mutex<Perform<String>>> = Lazy::new(|| {
-        let perform = Perform::None;
-        Mutex::new(perform)
-    });
 
-    pub async fn wasm_future_init() -> anyhow::Result<()> {
-        let mut m = INSTANCE.lock().await;
-        *m = Perform::Yet("yet".to_string());
+    static INSTANCE: Lazy<Mutex<Option<String>>> = Lazy::new(|| Mutex::new(None));
+
+    pub async fn init() -> Result<()> {
+        let mut lock = INSTANCE.lock().await;
+        *lock = None;
         Ok(())
     }
-
-    pub async fn wasm_future_pull() -> anyhow::Result<()> {
+    pub async fn pull() -> Result<()> {
+        use super::Ip;
         let response = reqwest::get("http://httpbin.org/ip")
             .await?
             .json::<Ip>()
             .await?;
-        let mut m = INSTANCE.lock().await;
-        *m = Perform::Already(response.origin);
+        let mut lock = INSTANCE.lock().await;
+        *lock = Some(response.origin);
         Ok(())
     }
-    pub async fn wasm_future_get() -> Option<String> {
-        let l = INSTANCE.lock().await;
-        let m: Perform<String> = l.clone();
-        From::from(m)
+    pub async fn get() -> Option<String> {
+        let lock = INSTANCE.lock().await;
+        lock.clone()
     }
 }
 
 fn run() {
-    let fut = || async move {
-        assert!(space::wasm_future_get().await.is_none());
-        assert!(space::wasm_future_init().await.is_ok());
-        assert!(space::wasm_future_get().await.is_none());
-        assert!(space::wasm_future_pull().await.is_ok());
-        let s = space::wasm_future_get().await;
-        assert!(s.is_some());
-        debug!("ip.origin: {:?}", s);
-    };
-
-    wasm_bindgen_futures::spawn_local(fut());
-}
-
-fn main() {
     console_error_panic_hook::set_once();
 
     wasm_logger::init(wasm_logger::Config::default());
@@ -84,7 +45,17 @@ fn main() {
     warn!("some warn log");
     error!("some error log");
 
-    run()
+    let fut = || async move {
+        assert!(space::get().await.is_none());
+        assert!(space::init().await.is_ok());
+        assert!(space::get().await.is_none());
+        assert!(space::pull().await.is_ok());
+        let origin = space::get().await;
+        assert!(origin.is_some());
+        debug!("ip.origin: {:?}", origin);
+    };
+
+    wasm_bindgen_futures::spawn_local(fut());
 }
 
 use wasm_bindgen_test::wasm_bindgen_test;
@@ -93,16 +64,9 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 async fn first_test() {
-    console_error_panic_hook::set_once();
-
-    wasm_logger::init(wasm_logger::Config::default());
-
-    trace!("some trace log");
-    debug!("some debug log");
-    info!("some info log");
-    warn!("some warn log");
-    error!("some error log");
-
     run();
-    assert!(true);
+}
+
+fn main() {
+    run();
 }
